fac36b8bcf8310e58f798aa337a56bb1c293d9cd
catherine <catmorillo@hotmail.com>
2023-01-18 11:11:38 -0500

    commit tree 304fa301a6dab6c7b489e22db95580b979d50b46

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-// write your code here
+
+document.addEventListener("DOMContentLoaded"),
+
+
+fetch (`http://localhost:3000/${/ramens/}`)
+.then(response => response.json())
+.then(ramensdata => renderRamens(ramensdata))
+
+method: 'GET',
+headers: {
+   'Content-type': 'application/json'
+}
+body: JSON.stringify(ramens)
+
+function renderRamens(ramens)
+
+  ramens.forEach(ramens)=>{}
+
+  renderHeader(ramens);
+  renderFooter(ramens); 
+  
+    const ramen =('document.querySelector("#ramen-menu")')
+    const menu = document.createElement('div') 
+    menu.className = 'ramen-menu'
+
+    ramenMenu.append(menu)
+    const img = document.createElement('img')
+    img.src = src="./assets/image-placeholder.jpg"
+    img.className = 'ramen-img'
+    menu.append(img)
+
+
+
+
+
+
+    
+
+
+
+
+
+
+   
+
+//.catch((error) => console.log(error));
